b3a6a00f2eff2d79a9e6c581eb8eaabfe99515d3
Juho Leinonen <juholei@gmail.com>
2017-12-30 17:38:08 +0200

    Add secret that needs to be in the request in order to save lunch time to db

--- src/lunch_lambda/core.cljs
+++ src/lunch_lambda/core.cljs
@@ -1,15 +1,23 @@
 (ns lunch-lambda.core
   (:require [cljs-lambda.macros :refer-macros [defgateway]]
             [eulalie.creds :as creds]
+            [eulalie.util :as eu]
             [cognitect.transit :as t]
             [lunch-lambda.dynamo :as db]))
 
 (def reader (t/reader :json))
 (def credentials (assoc (creds/env) :region "eu-central-1"))
+(def secret-key "LUNCH_SECRET")
+
+(defn save [credentials event body secret]
+  (let [{:keys [start end request-secret]} body]
+    (if (= request-secret secret)
+      (do
+        (db/add-lunch! credentials start end)
+        {:status  200
+         :headers {:content-type (-> event :headers :content-type)}
+         :body    (:body event)})
+      {:status 403})))
 
 (defgateway save-lunch [event ctx]
-  (let [{:keys [start end]} (t/read reader (:body event))]
-    (db/add-lunch! credentials start end)
-    {:status  200
-     :headers {:content-type (-> event :headers :content-type)}
-     :body    (event :body)}))
+  (save credentials event (t/read reader (:body event)) (eu/env! secret-key)))
--- test/lunch_lambda/core_test.cljs
+++ test/lunch_lambda/core_test.cljs
@@ -1,5 +1,17 @@
 (ns lunch-lambda.core-test
-  (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]))
+  (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
+            [lunch-lambda.core :as core]))
 
-(deftest test-test
-  (is (= true true)))
+(deftest test-request-has-no-secret
+  (is (= {:status 403}
+         (core/save {} {} {} "secrets"))))
+
+(deftest test-request-has-secret-saving-allowed
+  (let [response (core/save {} {} {:request-secret "secrets"} "secrets")]
+    (is (= 200
+           (:status response)))))
+
+(deftest test-there-is-no-secret-saving-allowed
+  (let [response (core/save {} {} {} nil)]
+    (is (= 200
+           (:status response)))))
